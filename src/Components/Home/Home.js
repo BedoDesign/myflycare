66233cf23e7fb4294bf10025722b381f8d991c1a
BedoDesign <Contact@bedo.agency>
2024-03-01 19:41:53 +0530

    Pushing Code

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Home/home.css'
+import banner from '../images/banner.jpeg'
+
+function Home() {
+    return (
+        <div>
+            <img src={banner} class="img-fluid" alt="..."></img>
+        </div>
+    )
+}
+
+export default Home
